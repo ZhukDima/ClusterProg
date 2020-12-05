773c57164e99c3c11f12d1c7a7f148055ac8fa1e
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-12-06 00:35:34 +0300

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include "FileReader.h"
 
 using namespace std;
 
 int main() {
-    FileReader a("../src/FileReader.cpp");
-    while (a.hasNextWord()) {
-        cout << a.getNextWord() << "\n";
-    }
+    cout << "ClusterProg\n";
     return 0;
 }
