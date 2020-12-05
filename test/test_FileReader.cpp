abba0532419ef7bde4c4c211ff4e9b3116cd0332
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-12-05 15:00:22 +0300

    Added first test

--- test/test_FileReader.cpp
+++ test/test_FileReader.cpp
@@ -1,8 +1,15 @@
 #include <fstream>
 #include <string>
+#include <iostream>
 #include "gtest/gtest.h"
 #include "FileReader.h"
 
 TEST(constructor, failed) {
-    EXPECT_THROW(FileReader("vabalabadabdab.txt"), char*);
+    EXPECT_THROW(FileReader("vabalabadabdab.txt"), const char*);
+    try {
+        FileReader a("vabalabadabdab.txt");
+    } catch (const char* err) {
+        std::string eq = "Unable to read file";
+        EXPECT_EQ(eq.compare(err), 0);
+    }
 }
