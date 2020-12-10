74da7a106bf880872a3041f6b457826ccd2e177f
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-12-10 17:43:48 +0300

    Check for existing dir

--- src/FileMover.cpp
+++ src/FileMover.cpp
@@ -7,6 +7,11 @@
 #include "FileMover.h"
 
 void FileMover::createDir(std::string path) {
+    DIR *dir = opendir(path.c_str());
+    if (dir != NULL) {
+        closedir(dir);
+        return;
+    }
     const int dir_err = mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     if (dir_err == -1) {
         throw "Unable to create dir " + path;
