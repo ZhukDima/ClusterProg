6055d5bab4ce04fcd51321e48b865ccc6e1bcddc
Vadim Khizhnyakov <provcydia@gmail.com>
2020-12-28 20:14:41 +0300

    edited because of cppcheck

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -3,27 +3,22 @@
 #include <iostream>
 #include "FileManager.h"
 
-namespace fs = std::filesystem;
+using namespace std::filesystem;
 
 // PUBLIC
 
-void FileManager::moveFile(std::string_view pathToSource, std::string pathToDestination)
-{
-    try
-    {
+void FileManager::moveFile(std::string_view pathToSource, std::string pathToDestination) {
+    try {
         pathToDestination = getNewFilePathIfExists(pathToDestination);
-        fs::rename(pathToSource, pathToDestination);
+        rename(pathToSource, pathToDestination);
     }
-    catch (const fs::filesystem_error &e)
-    {
+    catch (const filesystem_error &e) {
         throw e;
     }
 }
 
-void FileManager::createDir(std::string_view path)
-{
-    if (!fs::create_directories(path) && !fs::exists(path))
-    {
+void FileManager::createDir(std::string_view path) {
+    if (!create_directories(path) && !exists(path)) {
         throw std::runtime_error("Can't create directory on this path");
     }
 }
@@ -32,19 +27,18 @@ void FileManager::createDir(std::string_view path)
 
 const int FileManager::SAME_FILE_INDEX = 0;
 
-std::string FileManager::getNewFilePathIfExists(std::string_view path, const int idx)
-{
+std::string FileManager::getNewFilePathIfExists(std::string_view path, const int idx) {
     std::string newPath = constructNewPath(path, idx);
-    if (fs::exists(newPath))
-    {
+    if (exists(newPath)) {
         return getNewFilePathIfExists(path, idx + 1);
     }
     return newPath;
 }
 
-std::string FileManager::constructNewPath(std::string_view path, const int idx)
-{
-    fs::path oldPath(path);
-    fs::path newPath = oldPath.parent_path() / (fs::path(oldPath.stem().string() + (idx == FileManager::SAME_FILE_INDEX ? "" : "_" + std::to_string(idx)) + oldPath.extension().string()));
+std::string FileManager::constructNewPath(std::string_view _path, const int idx) {
+    path oldPath(_path);
+    path newPath = oldPath.parent_path() / (path(oldPath.stem().string() +
+                        (idx == FileManager::SAME_FILE_INDEX ? "" : "_" + std::to_string(idx)) +
+                        oldPath.extension().string()));
     return newPath.string();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    cout << "Hello, world!\n";
+    std::cout << "Hello, world!\n";
     return 0;
 }
